ef6d3a8dcc57b488c1f3d2c1725a4d57627088be
Maya Bowman <mayashady1@gmail.com>
2019-07-31 09:01:05 -0400

    fixed base url and other js issues

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
 'use strict';
 
-const apiKey = 'CUT1cfjFPFLSQvgzxBUV1BEgKD46xryTkDVr7pmk';
-const baseURL = 'https://developer.nps.gov/api/v1';
-const stateArr = $('#user-search').val().split(',');
-const maxResults = $('#user-max').val();
-
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
-function displayResults(responseJson) {
+function displayResults(responseJson, maxResults) {
   console.log(responseJson);
-  $('#results-list').html('');
-  for (let i = 0; i < responseJson.length; i++) {
-    $('#results-list').append(
-      `<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</h3>
-      <p>${responseJson.data[i].description}</p>
-      </li>`);
+  // clear previous results
+  $('#error-message').empty();
+  $('#results').empty();
+  // loop through response and insert results
+  for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+    $('#results').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+    <p>${responseJson.data[i].description}</p>
+    </li>`);
   }
   $('#results').removeClass('hidden');
 }
 
 function getParks(baseURL, stateArr, maxResults, apiKey) {
+  // set up parameters
   const params = {
     stateCode: stateArr,
     limit: maxResults
   }
+  // create url string
   const queryString = formatQueryParams(params);
   const url = baseURL + '?' + queryString + '&api_key=' + apiKey;
-
   console.log(url);
-  console.log(stateArr);
 
   fetch(url)
   .then(response => {
     if (response.ok) {
       return response.json();
-    } throw new Error(response.statusText);
+    }
+    throw new Error(response.statusText);
   })
   .then(responseJson => displayResults(responseJson, maxResults))
   .catch(error => {
@@ -46,13 +43,17 @@ function getParks(baseURL, stateArr, maxResults, apiKey) {
   });
 }
 
+// watch form for submit
 function watchForm() {
   $('#search-form').on('submit', function() {
     event.preventDefault();
-    // const stateArr = $('#user-search').val().split(',');
-    // const maxResults = $('#user-max').val();
+    const baseURL = 'https://api.nps.gov/api/v1/parks'
+    const stateArr = $('#user-search').val().split(",");
+    const maxResults = $('#user-max').val();
+    const apiKey = 'CUT1cfjFPFLSQvgzxBUV1BEgKD46xryTkDVr7pmk';
     getParks(baseURL, stateArr, maxResults, apiKey);
   })
 }
 
 $(watchForm);
+
